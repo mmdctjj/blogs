9be2a6eb972e3388719f949c15998e99f17ee9bd
mmdctjj <jiangpengjie@bizseer.com>
2023-07-17 07:10:40 +0800

    add token

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:mmdctjj/blogs.git master:gh-pages
+# git push -f git@github.com:mmdctjj/blogs.git master:gh-pages
 # git push -f https://github.com/mmdctjj/blogs.git master:gh-pages
-
+git push -f "https://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@github.com/mmdctjj/blogs.git" master:gh-pages
 cd -
